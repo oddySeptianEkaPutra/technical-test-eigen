69d9b04b5f22312299ff78a42b8dcf3dfb5c1e8f
oddySeptianEkaPutra <oddyseptian28@gmail.com>
2022-08-23 16:28:37 +0700

    complete algoritma - soal 3

--- algoritma/soal3.js
+++ algoritma/soal3.js
@@ -4,16 +4,15 @@ const input = ['xc', 'dz', 'bbb', 'dz'];
 const query = ['bbb', 'ac', 'dz'];
 
 function search(input, query) {
-  let counter = 0;
   let result = [];
   for (let i = 0; i < query.length; i++) {
+    let counter = 0;
     for (let j = 0; j < input.length; j++) {
       if(query[i] === input[j]) {
         counter = counter + 1;
       }
     }
     result.push(counter);
-    counter = 0;
   }
 
   return result;
