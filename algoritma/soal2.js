45c449ed5f1dabb19f1b07221eddafce1d99825d
oddySeptianEkaPutra <oddyseptian28@gmail.com>
2022-08-23 12:24:58 +0700

    complete algoritma - soal 2

---
+++ algoritma/soal2.js
@@ -0,0 +1,20 @@
+// soal 2
+
+const sentence = "Saya sangat senang mengerjakan soal algoritma";
+
+function longestWord(sentence) {
+  let splitWord = sentence.split(' ');
+  let longestWord = 0;
+  let word = '';
+
+  for (let i = 0; i < splitWord.length; i++) {
+    if(splitWord[i].length > longestWord) {
+      word = splitWord[i];
+      longestWord = splitWord[i].length;
+    }
+  }
+
+  return `${word}: ${longestWord} characters`;
+}
+
+console.log(longestWord(sentence));
